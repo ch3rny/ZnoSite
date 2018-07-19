c652a130820a16b168b75c0df074144dba952c5f
ch3rny <ch3rny777@gmail.com>
2018-07-19 22:24:55 +0300

    SQLite -> PostgreSQL

--- ZnoSite/settings.py
+++ ZnoSite/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'ZnoSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'znosite',
+        'USER': 'ch3rny',
+        'PASSWORD': '12qwaszx',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 # Password validation
